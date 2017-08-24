0974868a10de4c4192179a5a782a548d0aa417e2
Eduardo Aguilar <eduardoaguilartirado@gmail.com>
2017-08-24 13:44:47 +0200

    [FEAT] added needed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './features/recipes/recipes.component';
+import { RecipeListComponent } from './features/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './features/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './features/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShopComponent } from './features/shop/shop.component';
+import { ShoppingListComponent } from './features/shop/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './features/shop/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShopComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
